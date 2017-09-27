240c3596d1070ade72c6a5d8033e1bad02b4601d
井川司 <tsukasa.ikawa@secual-inc.com>
2017-09-27 15:21:05 +0900

    Swift3じゃないと動かないので、、、

--- Package.swift
+++ Package.swift
@@ -1,6 +1,3 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -8,14 +5,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
-        .package(url: "https://github.com/noppoMan/SwiftKnex.git", from: "0.2.0"),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "3.0.0")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "Meitnerium", dependencies: ["PerfectHTTPServer", "SwiftKnex", "SwiftyJSON"], path: "Sources"),
-        .testTarget(name: "MeitneriumTests", dependencies: ["Meitnerium"], path: "Tests"),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
+        .Package(url: "https://github.com/noppoMan/SwiftKnex.git", majorVersion: 0),
     ]
 )
